a134bf00e5fe3ef5f285f8ff16efe7b6144160c5
Charles Doutriaux <doutriaux1@llnl.gov>
2017-04-11 21:41:34 -0700

    updating mesa to latest version (#1)
    * updating mesa to latest version
    
    * duplicates removed
    
    * at last correct meta.yaml

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,12 +1,37 @@
 #!/bin/bash
 
+osname=`uname`
+if [ $osname == Linux ]; then
+    export CC="gcc"
+    export CXX="g++"
+elif [ $osname == Darwin ]; then
+    export CC="clang"
+    export CXX="clang++"
+fi
+
+export PKG_CONFIG_PATH="${PREFIX}/lib/pkgconfig/:"${PKG_CONFIG_PATH}
 export CFLAGS="-I${PREFIX}/include "${CFLAGS}
 export LDFLAGS="-L${PREFIX}/lib "${LDFLAGS}
 
-./configure  --prefix=$PREFIX \
-             --with-driver=osmesa \
-             --disable-egl \
-             --with-osmesa-bits=32
+./configure \
+    --prefix=$PREFIX \
+    --enable-opengl \
+    --disable-gles1 \
+    --disable-gles2 \
+    --disable-va \
+    --disable-gbm \
+    --disable-xvmc \
+    --disable-vdpau \
+    --enable-shared-glapi \
+    --enable-texture-float \
+    --disable-dri \
+    --with-dri-drivers= \
+    --with-gallium-drivers=swrast \
+    --disable-egl \
+    --with-egl-platforms= \
+    --enable-gallium-osmesa \
+    --disable-glx
 
 make -j${CPU_COUNT}
 make install
+
