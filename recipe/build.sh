243d3dbbff89f835436b9b89e1acd64029b3a272
Matthias Diener <mdiener@illinois.edu>
2021-11-23 10:36:59 -0600

    test

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,7 +6,7 @@ if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" ]]; then
   # Add pkg-config to cross-file binaries since meson will disable it
   # See https://github.com/mesonbuild/meson/issues/7276
   echo "[binaries]" >> $BUILD_PREFIX/meson_cross_file.txt
-  echo "pkg-config = '$(which pkg-config)'" >> $BUILD_PREFIX/meson_cross_file.txt
+  echo "pkg-config = $(which pkg-config)" >> $BUILD_PREFIX/meson_cross_file.txt
 
   cat $BUILD_PREFIX/meson_cross_file.txt
 fi
