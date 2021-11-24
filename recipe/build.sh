1e53adf6e363e3a3ef4035328ace609ab2c73a24
Mark Harfouche <mark.harfouche@gmail.com>
2021-11-24 11:02:44 -0500

    Try to build both version of the mesa lib

--- recipe/build.sh
+++ recipe/build.sh
@@ -21,3 +21,19 @@ ninja -C builddir/ install
 # meson test -C builddir/ \
 #   -t 4
 
+meson builddir/ \
+  -Dbuildtype=release \
+  -Dprefix=$PREFIX \
+  -Dlibdir=lib \
+  -Dplatforms=x11 \
+  -Dosmesa=true \
+  -Dosmesa-bits=32 \
+  -Dvulkan-drivers=[] \
+  -Dgallium-drivers=swrast \
+  -Ddri-drivers=[] \
+  -Dllvm=false
+
+ninja -C builddir/ -j ${CPU_COUNT}
+
+ninja -C builddir/ install
+
