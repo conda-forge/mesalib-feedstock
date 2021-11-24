3bd02a4d29a3bbe8c6336fd91caa2f033568628f
Mark Harfouche <mark.harfouche@gmail.com>
2021-11-24 11:24:09 -0500

    Rebuild OS mesa

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,7 +8,6 @@ meson builddir/ \
   -Dlibdir=lib \
   -Dplatforms=x11 \
   -Dosmesa=true \
-  -Dosmesa-bits=8 \
   -Dvulkan-drivers=[] \
   -Dgallium-drivers=swrast \
   -Ddri-drivers=[] \
@@ -20,20 +19,4 @@ ninja -C builddir/ install
 
 # meson test -C builddir/ \
 #   -t 4
-
-meson builddir/ \
-  -Dbuildtype=release \
-  -Dprefix=$PREFIX \
-  -Dlibdir=lib \
-  -Dplatforms=x11 \
-  -Dosmesa=true \
-  -Dosmesa-bits=32 \
-  -Dvulkan-drivers=[] \
-  -Dgallium-drivers=swrast \
-  -Ddri-drivers=[] \
-  -Dllvm=false
-
-ninja -C builddir/ -j ${CPU_COUNT}
-
-ninja -C builddir/ install
 
