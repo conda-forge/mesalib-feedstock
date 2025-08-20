1e29cac774b0494cb53c8ab474cda161d59dceea
Mark Harfouche <mark.harfouche@gmail.com>
2025-08-20 04:23:04 -0400

    add echo on, exit when failures, and meson_args for windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,8 +1,9 @@
+@echo on
 @REM microsoft-experimental added for required DirectX-headers
 
 meson setup builddir ^
+  %MESON_ARGS% ^
   --prefix=%LIBRARY_PREFIX% ^
-  --buildtype=release ^
   -Dplatforms=windows ^
   -Dgles1=disabled ^
   -Dgles2=disabled ^
@@ -18,11 +19,14 @@ meson setup builddir ^
   -Dvulkan-drivers=swrast,microsoft-experimental ^
   -Dopengl=true ^
   -Dglx-direct=false
+if %ERRORLEVEL% neq 0 exit 1
 
 @REM As of Aug 2025, LLVM doesn't not have support for shared libs on Windows
 @REM See https://github.com/conda-forge/llvmdev-feedstock/issues/237
 @REM -Dshared-llvm=enabled ^
 
 meson compile -C builddir
+if %ERRORLEVEL% neq 0 exit 1
 
 ninja -C builddir install
+if %ERRORLEVEL% neq 0 exit 1
